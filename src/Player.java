e9591083c2448a6073053c3294bfad8dae15e665
s3728207 <37423145+s3728207@users.noreply.github.com>
2019-03-22 10:09:42 +1100

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.awt.event.KeyEvent;
 
 public class Player {
 	private char keypress;
@@ -9,47 +10,69 @@ public class Player {
 		y = height;
 		energy = ene;
 	}
-	
+
 	public void action() {
 		if (keypress == 'L' && pressed) {
-			x -= 1;    // to be changed
+			x -= 1; // to be changed
 		}
 		if (keypress == 'R' && pressed) {
-			x += 1;    // to be changed
+			x += 1; // to be changed
 		}
 		if (keypress == 'U' && pressed) {
-			y -= 1;    // to be changed
+			y -= 1; // to be changed
 		}
 		if (keypress == 'D' && pressed) {
-			y += 1;    // to be changed
+			y += 1; // to be changed
 		}
 		if (keypress == 'S' && pressed) {
-			useSkill();    // to be improved
+			useSkill(); // to be improved
 		}
 	}
-	
+
 	public void useSkill() {
 		// placeholder
 	}
-	
+
 	public void die() {
 		// placeholder
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 	public int getEnergy() {
 		return energy;
 	}
-	
+
 	public void setKey(char key, Boolean press) {
 		keypress = key;
 		pressed = press;
+	}
+
+	public void keyPressed(KeyEvent ke) {
+		if (ke.getKeyCode() == KeyEvent.VK_LEFT)
+			this.setKey('L', true);
+		if (ke.getKeyCode() == KeyEvent.VK_RIGHT)
+			this.setKey('R', true);
+		if (ke.getKeyCode() == KeyEvent.VK_UP)
+			this.setKey('U', true);
+		if (ke.getKeyCode() == KeyEvent.VK_DOWN)
+			this.setKey('D', true);
+	}
+
+	public void keyReleased(KeyEvent ke) {
+		if (ke.getKeyCode() == KeyEvent.VK_LEFT)
+			this.setKey('L', false);
+		if (ke.getKeyCode() == KeyEvent.VK_RIGHT)
+			this.setKey('R', false);
+		if (ke.getKeyCode() == KeyEvent.VK_UP)
+			this.setKey('U', false);
+		if (ke.getKeyCode() == KeyEvent.VK_DOWN)
+			this.setKey('D', false);
 	}
 }
