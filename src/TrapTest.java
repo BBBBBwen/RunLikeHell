ce171233e338c4cdeb5f00d827200cc5cbebe8b3
Cheng Chen <s3728207@student.rmit.edu.au>
2019-04-11 20:27:29 +1000

    Create TrapTest.java

---
+++ src/TrapTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TrapTest {
+	private static int initXCor, initYCor;
+	private static Trap trap;
+
+	@BeforeAll
+	static void setup() {
+		initXCor = 10;
+		initYCor = 10;
+	}
+
+	@Test
+	void testDecTime() {
+		trap = new Trap(initXCor, initYCor);
+		
+		// Actions
+		trap.decTime();
+		
+		// Actual Results
+		int actualTime = trap.getTime();
+		
+		// Expected Results
+		int expectedTime = 9;
+
+		// Assertions
+		assertEquals(expectedTime, actualTime);
+	}
+	
+	@Test
+	void testDeactivate() {
+		trap = new Trap(initXCor, initYCor);
+		
+		// Actions
+		trap.deactivate();
+		
+		// Actual Results
+		boolean actualState = trap.getState();
+		
+		// Expected Results
+		boolean expectedState = false;
+		
+		// Assertions
+		assertEquals(actualState, expectedState);
+	}
+
+}
