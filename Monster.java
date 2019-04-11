88252a506d0475329dec3909359ca21dfdab7803
s3665803 <s3665803@student.rmit.edu.au>
2019-04-12 09:23:17 +1000

    Add files via upload

---
+++ Monster.java
@@ -0,0 +1,81 @@
+
+public class Monster {
+
+	private boolean hidden = false;
+	private int y;
+	private int x;
+	private int Mspeed;
+
+
+	public Monster(int initialX, int initialY, int initialMspeed) {
+		x = initialX;
+		y = initialY;
+		this.Mspeed = initialMspeed;
+	}
+
+	public void action() {
+		
+		}
+	
+
+	public void moveUp(int presses) {
+		if (presses == 1) {
+			y -= 1;
+		}
+	}
+
+	public void moveDown(int presses) {
+		if (presses == 1) {
+			y += 1;
+
+		}
+	}
+
+	public void moveLeft(int presses) {
+		if (presses == 1) {
+			x -= 1;
+
+		}
+	}
+
+	public void moveRight(int presses) {
+		if (presses == 1) {
+			x += 1;
+
+		}
+	}
+
+	public int getX()
+
+	{
+		return x;
+	}
+
+	public int getY()
+
+	{
+		return y;
+	}
+
+	public void hidden() {
+    
+     hidden= true;
+	
+	}
+    public void unhid(){
+	
+	
+	hidden= false;
+}
+
+
+
+public boolean getHidden() {
+	
+	return hidden;
+	
+}
+
+	
+	
+}
---
+++ TestMonster.java
@@ -0,0 +1,89 @@
+package test;
+
+
+import static org.junit.Assert.*;
+
+
+import org.junit.After;
+
+
+import org.junit.Before;
+
+
+import org.junit.Test;
+
+import model.Monster;
+
+
+
+public class TestMonster {
+
+
+	
+
+
+
+Monster M;
+
+
+
+	@Before
+
+
+	public void setUP()
+
+
+	{
+
+
+		M = new Monster(1,1);
+
+
+		
+
+
+		//x=1;
+
+
+		//y=1;
+
+
+	}
+
+
+
+	@Test
+
+
+	public void Move() {
+
+
+		M.Move();
+
+
+		assertEquals(2,M.getX());
+
+
+	}
+
+
+	@Test
+
+
+	public void notMove() {
+
+
+	//	M.notMove();
+
+
+		//boolean Nmove = M.notMove("")
+
+
+		assertNotEquals(false,M.notMove());
+
+
+	}
+
+
+
+}
