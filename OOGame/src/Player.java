73912f51f6c566b7d7b1596094391af660a0792d
Cheng Chen <s3728207@student.rmit.edu.au>
2019-05-30 18:13:42 +1000

    energy

--- OOGame/src/Player.java
+++ OOGame/src/Player.java
@@ -17,16 +17,22 @@ public class Player extends Moveable {
 	}
 
 	public Cell move(int presses) {
-		currentCell = grid.getCell(currentCell, currentDirection, presses);
-		if (presses == 1) {
+		boolean canMove = false;
+		if (presses == 1 && energy >= 2) {
 			energy -= 2;
-		} else if (presses == 2) {
+			canMove = true;
+		} else if (presses == 2 && energy >= 6) {
 			energy -= 6;
-		} else if (presses == 3) {
+			canMove = true;
+		} else if (presses == 3 && energy >= 14) {
 			energy -= 14;
+			canMove = true;
 		}
-		if (presses != 0) {
-			allowPut();
+		if (canMove) {
+			currentCell = grid.getCell(currentCell, currentDirection, presses);
+			if (presses != 0) {
+				allowPut();
+			}
 		}
 		clearPress();
 		return currentCell;
@@ -39,12 +45,12 @@ public class Player extends Moveable {
 	public int getEnergy() {
 		return energy;
 	}
-	
+
 	public ArrayList<Roadblock> getBlock() {
 		return roadblocks;
 	}
-	
-	public ArrayList<Trap> getTrap(){
+
+	public ArrayList<Trap> getTrap() {
 		return traps;
 	}
 
@@ -63,7 +69,7 @@ public class Player extends Moveable {
 	}
 
 	public void putTrap() {
-		if (canPutTrap) {
+		if (canPutTrap && energy >= 50) {
 			traps.add(new Trap(grid, currentCell));
 			energy -= 50;
 			canPutTrap = false;
@@ -71,12 +77,10 @@ public class Player extends Moveable {
 	}
 
 	public void putBlock() {
-		if (canPutBlock) {
-			if (roadblockPut < 3) {
-				roadblocks.add(new Roadblock(grid, currentCell));
-				roadblockPut += 1;
-				canPutBlock = false;
-			}
+		if (canPutBlock && roadblockPut < 3) {
+			roadblocks.add(new Roadblock(grid, currentCell));
+			roadblockPut += 1;
+			canPutBlock = false;
 		}
 	}
 
